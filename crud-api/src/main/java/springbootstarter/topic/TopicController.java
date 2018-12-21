df654f226dd318b9b4af6660875557462bc969fc
i-ayush <ayushjaiswal76@gmail.com.com>
2018-12-21 08:30:32 -0500

    Added update and delete method

--- crud-api/src/main/java/springbootstarter/topic/TopicController.java
+++ crud-api/src/main/java/springbootstarter/topic/TopicController.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +23,29 @@ public class TopicController {
 		return topicService.getAllTopics();
 	}
 
+	
+	@RequestMapping("topics/{id}")
+	public Topic getTopic(@PathVariable String id)
+	{
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/topics")
+	public void addTopic(@RequestBody Topic topic)
+	{
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,value="/topics/{id}")
+	public void addTopic(@RequestBody Topic topic, @PathVariable String id)
+	{
+		topicService.updateTopic(id,topic);
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.DELETE,value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id)
+	{
+		topicService.deleteTopic(id);
+	}
 }
