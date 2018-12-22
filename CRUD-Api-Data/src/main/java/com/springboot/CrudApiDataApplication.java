ccc419072d0b52062e5e337dca6ed74aadb19140
i-ayush <ayushjaiswal76@gmail.com.com>
2018-12-22 13:28:43 -0500

    Adding new CRUD-APi project using Database

---
+++ CRUD-Api-Data/src/main/java/com/springboot/CrudApiDataApplication.java
@@ -0,0 +1,14 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiDataApplication.class, args);
+	}
+
+}
+
